5a3aa269c7678a5aa98b4f95029c34ddffa4e0b8
AlexMasonAM <alexmmason@gmail.com>
2015-02-03 17:31:47 -0800

    finished setting up Rspec and added User Rspec tests

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email Faker::Internet.email
+    password "password"
+    password_confirmation "password"
+    yelp_id Faker::Bitcoin.address
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){FactoryGirl.build(:user)}
+
+  it "should respond to first_name" do
+    expect(user).to respond_to("first_name")
+  end
+  
+  it "should validate presence of first_name" do
+    user.first_name = nil
+    expect(user).to be_invalid
+  end
+
+  it "should respond to last_name" do
+    expect(user).to respond_to("last_name")
+  end
+
+  it "should validate presence of last_name" do
+    user.last_name = nil
+    expect(user).to be_invalid
+  end
+
+  it "should respond to email" do
+    expect(user).to respond_to("email")
+  end 
+
+  it "should validate presence of email" do
+    user.email = nil
+    expect(user).to be_invalid
+  end
+
+  it "validates uniqueness of email" do
+    user.save
+    user2 = FactoryGirl.build(:user, email: user.email)
+
+    expect(user2).to be_invalid
+  end
+
+  it "validates presence of password and password_confirmation" do
+    user.password = nil
+    user.password_confirmation = nil
+    
+    expect(user).to be_invalid
+  end
+
+  it "validates that password and password confirmation match" do
+    user.password_confirmation = "not password"
+
+    expect(user).to be_invalid
+  end
+
+  it "should respond to yelp_id" do
+    expect(user).to respond_to("yelp_id")
+  end
 end
