931696206eef11804781fa05752263224ccdd418
jamesdmkim <bronze10000@gmail.com>
2015-02-04 19:27:30 -0800

    some Rspec on marker model

--- spec/models/marker_spec.rb
+++ spec/models/marker_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Marker, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let(:marker){FactoryGirl.build(:marker)}
+
+  # it "has a latitude" do
+  #   expect(marker).to respond_to(:latitude)
+  # end
+  # it "has a longitude" do
+  #   expect(marker).to respond_to(:longitude)
+  # end
+
+  it "has an address" do
+    expect(marker).to respond_to(:address)
+  end
+
+  # it "has an end_time" do
+  #   expect(marker).to respond_to(:end_time)
+  # end
+
+
+
+
 end
--- spec/models/truck_spec.rb
+++ spec/models/truck_spec.rb
@@ -3,3 +3,5 @@ require 'rails_helper'
 RSpec.describe Truck, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+end
