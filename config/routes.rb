681321be0fe71c0ae3abbe97c3f533a0d1472182
ramiburpee <rburpee@gmail.com>
2015-02-09 22:08:36 -0800

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
     resources :favorites, :markers, :trucks, :truck_accounts, :users
   end
 
+
+  # these might no longer be needed
   resources :truck_accounts, except: [:index] do
     resources :trucks, only: [:index, :create, :update, :destroy]
   end
-
   resources :trucks, only: [:index, :show]
-
   resources :users
   resources :favorites
   resources :markers
