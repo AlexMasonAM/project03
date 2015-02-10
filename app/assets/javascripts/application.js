081e3b35d07e1d37153264f4d89beee764dd3af0
AlexMasonAM <alexmmason@gmail.com>
2015-02-09 19:10:34 -0800

    incorporated Angular for single page functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require angular
+//= require angular-animate
+//= require angular-ui-router
+//= require angular-rails-templates
+//= require angular-resource
 //= require_tree .
+//= require_tree ../templates
