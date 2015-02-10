e1e4bee6e982a95752d40193db31415805f321c7
ramiburpee <rburpee@gmail.com>
2015-02-09 22:22:53 -0800

    Update json output for markers#index to include some truck info

--- app/controllers/api/markers_controller.rb
+++ app/controllers/api/markers_controller.rb
@@ -1,6 +1,8 @@
 class Api::MarkersController < ApplicationController
   def index
     markers = Marker.all
-    render json: markers
+    render json: markers, 
+           except: [:id, :truck_id, :updated_at],
+           include: {truck: {only: [:name, :genre, :twitter_handle, :tweet_most_recent]}}
   end
 end
