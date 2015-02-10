8c27f380177bb009676f85fba7a1b87c4c6f36ea
jamesdmkim <bronze10000@gmail.com>
2015-02-10 14:59:08 -0800

    added actions to api/users_controller.rb

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,59 @@
 class Api::UsersController < ApplicationController
 
+  def create
+    user = User.new(post_params)
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors}, status: 422
+    end
+  end
 
+  def index
+    users = User.all
+    render json: users,
+           except: [:updated_at, :created_at, :password_digest, :id],
+           include: {
+                    # favorites: { except: []}, 
+                    trucks: {except: [:created_at, :updated_at]},
+                    # preference_list: {except: []}
+                    }
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user,
+           except: [:updated_at, :created_at, :password_digest, :id],
+           include: {
+                    # favorites: { except: []}, 
+                    trucks: {except: [:created_at, :updated_at]},
+                    # preference_list: {except: []}
+                    }
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: {errors: user.errors}, status: 422
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      render json: user, status: 200
+    else
+      render json: user, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :first_name, :last_name, :email, :password_digest, :yelp_id)
+  end
 end
 
 
