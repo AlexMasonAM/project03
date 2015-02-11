f963b85209e5ec601d731839afb7847dc72f31fb
ramiburpee <rburpee@gmail.com>
2015-02-10 22:47:10 -0800

    Update favorites controller rspec tests, and update api controller to make them pass

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,4 +1,8 @@
 class Api::FavoritesController < ApplicationController
 
+  def index
+    favorites = Favorite.all
+    render json: favorites
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,5 @@ Rails.application.routes.draw do
   end
 
 
-  # these might no longer be needed
-  # resources :truck_accounts, except: [:index] do
-  #   resources :trucks, only: [:index, :create, :update, :destroy]
-  # end
-  # resources :trucks, only: [:index, :show]
-  # resources :users
-  # resources :favorites
-  # resources :markers
 
 end
