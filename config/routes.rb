a6764d35e2d031d0dd34a8b627821832626a856c
ramiburpee <rburpee@gmail.com>
2015-02-10 23:43:14 -0800

    Add routes for login/signup pages&actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,26 @@
 Rails.application.routes.draw do
 
+  # ng-app
   root "application#index"
 
+  # api routes
   namespace :api do
     resources :favorites, :markers, :trucks, :users, except: [:new, :edit]
 
     resources :truck_accounts, except: [:new, :edit] do
-      resources :trucks, except: [:new, :edit]
+      resources :trucks, except: [:new, :edit] do
+        resources :markers, only: [:create]
+      end
     end
   end
 
-
+  # signup / login routes
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get '/truck-signup' => 'truck_accounts#new'
+  post '/truck-signup' => 'truck_accounts#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 end
