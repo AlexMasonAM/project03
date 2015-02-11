1e620cdca8fbe81430ddf0db7cfbbde84f84a756
ramiburpee <rburpee@gmail.com>
2015-02-10 22:33:35 -0800

    Update user controller rspec tests and fix a couple bugs in the users controller

--- app/controllers/api/markers_controller.rb
+++ app/controllers/api/markers_controller.rb
@@ -23,31 +23,6 @@ class Api::MarkersController < ApplicationController
     end
   end
 
-
-
 end
 
-
-
-
-
-
-# class MarkersController < ApplicationController
-  
-#   def new
-#     @marker = Marker.new
-#   end
-
-#   def create
-#     @marker = Marker.new(params.require(:marker).permit(:latitude, :longitude, :address, :end_time))
-
-#     if @marker.save
-#       flash[:info] = "marker has been added to the map" 
-#       render :new
-#     else
-#       flash[:warning] = "marker wasn't added to the map"
-#       render :new
-#     end
-#   end
-# end
 
