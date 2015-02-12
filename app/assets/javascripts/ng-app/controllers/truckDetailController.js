77012eff8333a16c89f55cee44e78195f16a763d
ramiburpee <rburpee@gmail.com>
2015-02-11 23:13:23 -0800

    Add basic display for truck detail and link from map windows to truck details

--- app/assets/javascripts/ng-app/controllers/truckDetailController.js
+++ app/assets/javascripts/ng-app/controllers/truckDetailController.js
@@ -1,5 +1,9 @@
 angular
   .module('truckApp')
-  .controller('TruckDetailController', function(){
-    
+  .controller('TruckDetailController', function($resource, $stateParams){
+    var self = this;
+    var Truck = $resource('/api/trucks/:id', {id: '@id'});
+    Truck.get({id: $stateParams.truckid}, function(truck) {
+      self.truck = truck;
+    });
   });
