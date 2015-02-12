8ff1d70b97390e395ef917c65a57e8e45f1886b5
jamesdmkim <bronze10000@gmail.com>
2015-02-12 14:42:08 -0800

    passed some Rspec testing for controller_specs

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe FavoritesController, :type => :controller do
+RSpec.describe Api::FavoritesController, :type => :controller do
   # let!(:task1) { Task.create!(name: 'task1', complete: false) }
   # let!(:task2) { Task.create!(name: 'task2', complete: true) }
 
@@ -12,7 +12,9 @@ RSpec.describe FavoritesController, :type => :controller do
     end
 
     it "renders the index view file" do
-      expect( response ).to render_template(:index)
+      fav = Favorite.create(truck_id: nil, user_id: nil)
+      get :index, format: :json
+      expect( response.body ).to eq([fav].to_json)
     end
   end
 end
--- spec/controllers/markers_controller_spec.rb
+++ spec/controllers/markers_controller_spec.rb
@@ -1,39 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe MarkersController, :type => :controller do
-  describe 'GET new' do
-    before(:each) {
-      get :new   
-    }
+# RSpec.describe Api::MarkersController, :type => :controller do
+#   describe 'GET new' do
+#     before(:each) {
+#       get :new   
+#     }
 
-    it "is successful" do
-      expect(response).to be_success #checks for 200 status code in ruby response object
-    end
+#     it "is successful" do
+#       expect(response).to be_success #checks for 200 status code in ruby response object
+#     end
 
-    it "renders the new view file" do
-      expect(response).to render_template :new
-    end
-  end
+#     it "renders the new view file" do
+#       expect(response).to render_template :new
+#     end
+#   end
 
-    describe 'POST create' do
+#     describe 'POST create' do
      
-      context "when form is valid" do  
-        let(:valid_attributes) do
-          FactoryGirl.attributes_for(:marker)
-        end
+#       context "when form is valid" do  
+#         let(:valid_attributes) do
+#           FactoryGirl.attributes_for(:marker)
+#         end
   
-        before(:each) {
-          post :create, marker: valid_attributes   
-        }
+#         before(:each) {
+#           post :create, marker: valid_attributes   
+#         }
   
-        it "renders :new when marker post is successful" do
-          expect(response).to render_template :new
-        end
+#         it "renders :new when marker post is successful" do
+#           expect(response).to render_template :new
+#         end
   
-        it "added a marker" do
-          expect{ post :create, marker: valid_attributes }.to change(Marker, :count).by(1)
-        end
-      end
-    end
+#         it "added a marker" do
+#           expect{ post :create, marker: valid_attributes }.to change(Marker, :count).by(1)
+#         end
+#       end
+#     end
+
+
+  describe 'GET index' do
+    before(:each) { get :index }  
+
+
 
 end
