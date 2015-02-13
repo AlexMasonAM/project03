4b341455460fa81e0158dbe7a4fa83fc4c77615a
ramiburpee <rburpee@gmail.com>
2015-02-13 12:25:47 -0800

    Add pagination to truck index page and update controller to pull truck data

--- app/assets/javascripts/ng-app/controllers/truckController.js
+++ app/assets/javascripts/ng-app/controllers/truckController.js
@@ -1,5 +1,22 @@
 angular
   .module('truckApp')
-  .controller('TruckController', function(){
-    
+  .controller('TruckController', function($scope, $resource){
+    var self = this;
+    var Truck = $resource('/api/trucks/:id', {id: '@id'});
+
+    Truck.query(function(data){
+      self.trucks = data;
+
+      self.sixTrucks = self.trucks.slice(0, 6);
+
+    });
+
+    self.pageChanged = function() {
+      console.log(self.trucks);
+      var start = (self.currentPage - 1)*6;
+      self.sixTrucks = self.trucks.slice(start, start+6);
+      console.log(self.sixTrucks);
+    };
+
+
   });
