131d9115e496685277f98dc45d409c90ec0f3305
jamesdmkim <bronze10000@gmail.com>
2015-02-13 09:11:12 -0800

    added yelp gem

--- app/assets/javascripts/ng-app/controllers/truckDetailController.js
+++ app/assets/javascripts/ng-app/controllers/truckDetailController.js
@@ -1,9 +1,13 @@
 angular
   .module('truckApp')
-  .controller('TruckDetailController', function($resource, $stateParams){
+  .controller('TruckDetailController', function($resource, $stateParams, $http){
     var self = this;
     var Truck = $resource('/api/trucks/:id', {id: '@id'});
     Truck.get({id: $stateParams.truckid}, function(truck) {
       self.truck = truck;
+      console.log(self.truck.name);
+      $http.get("/api/yelp/search/" + "?term=" + self.truck.name).success( function(data,status) {
+      self.yelpdata = data  
+      })
     });
   });
