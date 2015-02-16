429671bff6905158e44b36dbb182dd6d1bd5e052
ramiburpee <rburpee@gmail.com>
2015-02-16 01:57:09 -0800

    Added ability to add truck as favorite

--- app/assets/javascripts/ng-app/controllers/truckDetailController.js
+++ app/assets/javascripts/ng-app/controllers/truckDetailController.js
@@ -5,15 +5,51 @@ angular
     function TruckDetailController($resource, $stateParams, $http){
     var self = this;
     var Truck = $resource('/api/trucks/:id', {id: '@id'});
-    // console.log("TRUCK ID:")
-    // console.log($stateParams.truckid)
-    Truck.get({id: $stateParams.truckid}, function(truck) {
-      console.log(truck)
-      self.truck = truck;
-      console.log(self.truck.name);
-      $http.get("/api/yelp/search/" + "?term=" + self.truck.yelp_id).success( function(data,status) {
-        self.yelpdata = data  
-      });
+    var Favorite = $resource('/api/favorites/:id', {id: '@id'});
+    var User = $resource('/api/users/:id', {id: '@id'});
 
+    self.isFavorite = false;
+
+    if(currentUser && currentUser.type == 'User') {
+      User.get({id: currentUser.id}, function(data) {
+        for(var i = 0; i < data.favorites.length; i++) {
+          if($stateParams.truckid == data.favorites[i].truck_id) {
+            self.isFavorite = true;
+            self.favoriteId = data.favorites[i].id;
+            self.post = " saved!";
+            Favorite.get({id: self.favoriteId}, function(fav_data) {
+              self.fav = fav_data;
+            });
+            break;
+          }
+        }
+
+      });
+    }
+
+    self.toggleFavorite = function() {
+      if(!self.isFavorite) {
+        self.fav = new Favorite({
+          truck_id: self.truck.id,
+          user_id: currentUser.id
+        });
+        self.fav.$save();
+        self.isFavorite = true;
+        self.post = " saved!";
+        self.pre = '';
+      } else {
+        self.fav.$delete({id: self.favoriteId});
+        self.isFavorite = false;
+        self.pre = "Add as ";
+        self.post = '';
+      }
+    };
+
+
+    Truck.get({id: $stateParams.truckid}, function(truck) {
+      self.truck = truck;
+      $http.get("/api/yelp/search/" + "?term=" + self.truck.yelp_id).success( function(data,status) {
+        self.yelpdata = data;  
+      });
     });
   }
--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -4,4 +4,29 @@ class Api::FavoritesController < ApplicationController
     favorites = Favorite.all
     render json: favorites
   end
+
+  def show
+    favorite = Favorite.find(params[:id])
+    render json: favorite
+  end
+
+  def create
+    favorite = Favorite.find_by(truck_id: params[:favorite][:truck_id], user_id: params[:favorite][:user_id])
+    if favorite
+      render json: favorite, status: 200
+    else
+      favorite = Favorite.create(params.require(:favorite).permit(:truck_id, :user_id))
+      render json: favorite
+    end
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:id])
+    if favorite.destroy
+      render json: favorite, status: 200
+    else
+      render json: favorite, status: 400
+    end
+  end
+
 end 
